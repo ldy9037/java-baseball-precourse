faa006f2605527b6be930da2830783e8bd266b2e
emadam <ldy9037@naver.com>
2022-09-29 08:18:13 +0900

    test(study): size메서드로 Set의 크기를 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,9 +1,13 @@
 package study;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class SetTest {
     
@@ -17,4 +21,14 @@ public class SetTest {
 		numbers.add(2);
 		numbers.add(3);	
 	}
+
+    @Test
+    @DisplayName("size() 메소드를 활용해 Set의 크기를 확인한다.")
+    void size_int() {
+        // when
+        int size = numbers.size();
+
+        // then
+        assertThat(size).isEqualTo(3);
+    }
 }
