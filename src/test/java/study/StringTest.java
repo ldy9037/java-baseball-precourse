4ebace995b83e15a26a6eba8379805c43112b34f
emadam <ldy9037@naver.com>
2022-09-29 08:08:58 +0900

    test(study): Java Code Convention 적용
    - Test 메서드명을 논리적 단위로 분리
    - import에 와일드 카드 제거

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,16 +1,17 @@
 package study;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.*;
-
 public class StringTest {
     @Test
     @DisplayName("1,2를 ,로 split 했을 때 배열 {1,2}로 분리된다.")
-    void splitString() {
+    void split_string_array() {
         // given
         String oneCommaTwo = "1,2";
         
@@ -23,7 +24,7 @@ public class StringTest {
 
     @Test
     @DisplayName("String 1을 ,로 split 했을 때 1만을 포함하는 배열이 반환된다.")
-    void splitMismatchedRegex() {
+    void split_mismatchedRegex_array() {
         // given
         String one = "1";
 
@@ -37,7 +38,7 @@ public class StringTest {
 
     @Test
     @DisplayName("String (1,2)를 1부터 4까지 subString 했을 때 1,2가 반환된다.")
-    void substringTo4From1() {
+    void substring_removeBracket_string() {
         // given 
         String OneAndTwo = "(1,2)";
         
@@ -51,7 +52,7 @@ public class StringTest {
     @ParameterizedTest(name = "index: {0}, expected: {1}")
     @CsvSource(value = {"1:b", "2:c", "0:a"}, delimiter = ':')
     @DisplayName("charAt()을 String abc의 특정 위치 문자를 가져온다.")
-    void charAt(int index, char expected) {
+    void charAt_char(int index, char expected) {
         // given
         String abc = "abc";
 
@@ -64,7 +65,7 @@ public class StringTest {
 
     @Test
     @DisplayName("charAt() 사용 시 위치 값을 벗어나면 에러가 발생한다.")
-    void charAtWithOutOfBounds() {
+    void charAt_useIndexOutOfBounds_exception() {
         // given
         String abc = "abc";
         
