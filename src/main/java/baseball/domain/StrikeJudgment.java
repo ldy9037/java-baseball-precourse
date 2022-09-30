9adcb813f223366ccc2d34dfa50c57d5bd79a0dd
emadam <ldy9037@naver.com>
2022-09-30 19:55:19 +0900

    feat(domain): 같은 수가 같은 자리에 있으면 스트라이크이다
    - 같은 위치(index)에 같은 숫자가 존재하면 Strike count를 증가시키는 테스트 작성
    - String 숫자 조합을 BaseballNumber List로 변환 시켜주는 메서드 구현
    - getCount 오퍼레이터를 가지고 있는 인터페이스 생성
    - 스트라이크를 판별하는 judge 메서드 구현
    - 판별한 count로 스트라이크 판정을 생성하는 정적 팩토리 메서드 생성
    
    https://emadam.notion.site/cf83134ec34d472caee0d02f54188a8b

---
+++ src/main/java/baseball/domain/StrikeJudgment.java
@@ -0,0 +1,41 @@
+package baseball.domain;
+
+public class StrikeJudgment implements Judgment{
+    
+    private final int count;
+    
+    private StrikeJudgment(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public static StrikeJudgment of(
+        BaseballNumberCombination computerCombination, 
+        BaseballNumberCombination playerCombination
+    ) {
+        return new StrikeJudgment(judge(computerCombination, playerCombination));
+    }
+
+    private static int judge(
+        BaseballNumberCombination computerCombination, 
+        BaseballNumberCombination playerCombination
+    ) { 
+        int result = 0;
+
+        for (int i = 0; i < BaseballNumberCombination.NUMBER_SIZE; i++) {
+            result += compare(computerCombination.getNumber(i), playerCombination.getNumber(i));
+        }
+
+        return result;
+    }
+    
+    private static int compare(
+        BaseballNumber computerNumber,
+        BaseballNumber playerNumber
+    ) {
+        return (computerNumber.equals(playerNumber)) ? 1 : 0;
+    }
+}
---
+++ src/test/java/baseball/domain/JudgmentTest.java
@@ -0,0 +1,37 @@
+package baseball.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class JudgmentTest {
+    
+    @ParameterizedTest(name = "computer: {0}, player: {1}, expected: {2}")
+    @CsvSource(value = {"423:123:2", "812:142:1", "124:732:0"}, delimiter = ':')
+    @DisplayName("같은 자리에 같은 숫자가 존재하면 스트라이크 카운트를 증가시킨다.")
+    void new_strikeJudgment(String combination1, String combination2, int expected){
+        // when
+        Judgment strikeJudgment = StrikeJudgment.of(
+                new BaseballNumberCombination(generateBaseballNumberList(combination1)),
+                new BaseballNumberCombination(generateBaseballNumberList(combination2)));
+        
+        // then
+        assertThat(strikeJudgment.getCount()).isEqualTo(expected);    
+    }
+
+    private List<BaseballNumber> generateBaseballNumberList(String combination) {
+        List<BaseballNumber> baseballNumbers = new ArrayList<>();
+
+        for (char number : combination.toCharArray()) {
+            baseballNumbers.add(new BaseballNumber(Character.getNumericValue(number)));
+        }
+
+        return baseballNumbers;
+    }
+
+}
