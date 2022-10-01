592da0a8375fdaf740d2d0cf70e062e8e32176ff
emadam <ldy9037@naver.com>
2022-10-01 16:30:35 +0900

    refactor(domain): game 객체 생성로직 BeforeEach로 분리

--- src/test/java/baseball/domain/GameTest.java
+++ src/test/java/baseball/domain/GameTest.java
@@ -2,18 +2,26 @@ package baseball.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
     
+    private Game game;
+    
+    @BeforeEach
+    private void setUp() {
+        game = new Game();
+    }
+    
     @Test
-    @DisplayName("게임을 상태를 확인한다.")
+    @DisplayName("게임 상태를 확인한다.")
     void new_game() {
-        // when 
-        Game game = new Game();
+        // when         
+        GameStatus gameStatus = game.getStatus();
 
         // then 
-        assertThat(game.getStatus()).isEqualTo(GameStatus.IN_PROGRESS);
+        assertThat(gameStatus).isEqualTo(GameStatus.IN_PROGRESS);
     }
 }
